4a02bb4391e60c23d5c5e9944008a8e08615e034
Abhilasha Mangal <Abhilasha.Mangal@ibm.com>
2022-10-31 22:55:00 +0530

    update TTS dash app

--- Dash-App/TTS_dash_app.py
+++ Dash-App/TTS_dash_app.py
@@ -9,7 +9,7 @@ import matplotlib.pyplot as plt
 import plotly.express as px
 import requests
 import librosa
-
+import base64
 
 plt.switch_backend('Agg') 
 
@@ -17,10 +17,12 @@ external_stylesheets = ['assets/bootstrap.min.css']
 app = dash.Dash(external_stylesheets=external_stylesheets)
 app.title = 'Watson Text to Speech (TTS) Library'
 
+
 # Setting theme for plotly charts
 plotly_template = pio.templates["plotly_dark"]
 pio.templates["plotly_dark_custom"] = pio.templates["plotly_dark"]
 app = dash.Dash(__name__, assets_folder=os.path.join(os.path.dirname(__file__), 'assets'))
+
 
 navbar_main = dbc.Navbar(
         [
@@ -79,17 +81,11 @@ text_to_speech_url = os.getenv("TTS_SERVICE_URL", default='http://0271714b-us-so
 headers = {"Content-Type": "application/json","Accept":"audio/wav"}
 params ={'voice':'en-US_AllisonV3Voice'}
 file_name = "assets/result.wav"
-tts_sample_text = "I don't share everyone's unbridled enthusiasm for this film. It is indeed a great popcorn flick, with outstanding aerial photography and maneuvers. But 10 stars? There are few, if any, movies that are perfect, and deserve that kind of rating. \
-The problem with the film is the plot. It is so filled with age-worn cliches that one could easily tell what was coming from beginning to end. I mean, you had to know who was going to save the day at the end, and you had to know what was going to happen when Maverick jumped out of Penny's window. Those are just two examples of the many obvious plot points that you could see coming a mile away. I could list them all, but it would take up too much space here. Basically the entire plot was entirely predictable. \
-The opening scene, especially, was straight out of Hollywood Screenplay Writing 101. I mean, seriously, how many times have we seen that subplot? Countless. \
-There were no characters in the movie, either. They were all caricatures, stereotypes. No depth to any of them. They had their standard roles to play, and that was it. \
-Did I enjoy the film? Sure, it was fun. Especially on a big theater screen with a loud sound system. Did I take anything away from the film? Did it make me think about anything after it was over? Nah. Will I see it again? Nah. \
-I will give Tom Cruise credit for including Val Kilmer in the cast. Considering his health problems, that was a nice touch. \
-So, yeah, enjoy the film. Sit back with your bag of popcorn and enjoy the g-forces. But don't pretend it is anything other than just another summer blockbuster."
+tts_sample_text = "Welcome to Watson Text to speech service demo. Watson Text to Speech service supports a wide variety of voices in all supported languages and dialects."
 
 tts_analysis_input =  dbc.InputGroup(
             [
-                dbc.Textarea(id="tts-input", value=tts_sample_text,cols=150,rows=8,persistence=True,persistence_type='session', placeholder="Text to Speech analysis"),
+                dbc.Textarea(id="tts-input", value=tts_sample_text,cols=150,rows=5,persistence=True,persistence_type='session', placeholder="Text to Speech analysis"),
                 # dbc.Textarea(id="tts-input", value=tts_sample_text,cols=150,rows=8, placeholder="Text to Speech analysis"),
                 dcc.Clipboard(
                     target_id="textarea-tts",
@@ -106,11 +102,14 @@ tts_analysis_input =  dbc.InputGroup(
             className="mb-3",
         )
 
+'''
 audio2 = html.Div(children=[
-    html.Audio(html.Source(src=file_name,type="audio/wav"), controls=True,style={ "width": "100%"})
+    html.Audio(html.Source(src=file_name,type="audio/wav"), controls=True,preload='auto',style={ "width": "100%"})
 ])
+'''
 
-image_path='assets/output.png'
+
+#image_path='assets/output.png'
 
 def print_plot_play(fileName, text=''):
     x, Fs = librosa.load(fileName, sr=None)
@@ -142,16 +141,15 @@ app.layout = html.Div(children=[
                             dbc.Col(html.P(children="Select option for Enhanced neural voice")),
                             dbc.Col(
                                 dcc.Dropdown(["Allison","Michael"], "Allison", id='voice_dropdown',persistence=True,persistence_type='session',style={'color':'#00361c'})
-                                #dcc.Dropdown(["Allison","Michael"], "Allison", id='voice_dropdown',style={'color':'#00361c'})
                             ),
                             dbc.Col(width=2),
                         ]
                         ),
                         html.Div(tts_analysis_input),
                         html.Br(),
-                        # html.Div(audio2),
                         html.Div(id="div-audio", children=[' ']),
                         html.Br(),
+                        #html.Audio(html.Source(src=file_name1,type="audio/wav"),id="audio1", controls=True,preload='auto',style={ "width": "100%"})
                         #html.P(children="Text To Speech Output wave form"),
                         html.Div(wave_figure),
                         #html.Img(src=image_path,style={ "width": "99%","height":"28%",'textAlign': 'center','margin-right':'100px'}),
@@ -160,44 +158,53 @@ app.layout = html.Div(children=[
                     ],
                 ),
                 html.Br(),
-                html.Label("This App was built using Watson NLP library."),
-                html.Br(),
+                html.Label("This App was built using Watson Speech library.",style={"bottom":"25px","position": "absolute"}),
+                #html.Br(),
                 html.Footer(children="Please note that this content is made available by IBM Build Lab to foster Embedded AI technology adoption. \
                                 The content may include systems & methods pending patent with USPTO and protected under US Patent Laws. \
-                                Copyright - 2022 IBM Corporation")
+                                Copyright - 2022 IBM Corporation",style={"bottom":"0px","position": "absolute"})
 ])
 
 # method to get the Voice data from the text service 
-def getSpeechFromText(headers,params,data,file_name,voice_dropdown):
+def getSpeechFromText(headers,params,data,voice_dropdown):
     if voice_dropdown =='Michael':
         params ={'voice':'en-US_MichaelV3Voice'}
     request =requests.post(text_to_speech_url,headers=headers,params =params,data=data)
     print(request.status_code)
-    print(request.content)
+    file_data = file_name
     if request.status_code != 200:
         print("TTS Service status:", request.text)
     if os.path.exists(file_name):
         os.remove(file_name)
-    with open(file_name, mode='bx') as f:
+    with open(file_data, mode='bx') as f:
         f.write(request.content)
+    return file_data
+        
 
 @app.callback(
     Output('div-audio', 'children'),
     Output('wave-figure', 'figure'),
     Input('tts-button', 'n_clicks'),
-    Input('voice_dropdown', 'value'),
+    State('voice_dropdown', 'value'),
     State('tts-input', 'value')
 )
+
 def update_output(n_clicks, voice_dropdown, text_input):
-    print("INPUT TEXT:", text_input)
-    print(voice_dropdown)
-    text_data = '{"text":"' + text_input + '"}'
-    file_name = 'assets/result.wav'
-    image_path ='assets/output.png'
-    getSpeechFromText(headers,params,text_data,file_name,voice_dropdown)
-    plt = print_plot_play(file_name, "Text To Speech Wav form")
-    #plt.savefig(image_path)
-    return audio2, plt
+        print("INPUT TEXT:", text_input)
+        print(voice_dropdown)
+        text_data = '{"text":"' + text_input + '"}'
+        file_data=getSpeechFromText(headers,params,text_data,voice_dropdown)
+        plt = print_plot_play(file_name, "Text To Speech Wav form")
+        #plt.savefig(image_path)
+        print("file_data---",file_data)
+        data_sound = base64.b64encode(open(file_data, 'rb').read())
+        audio3 = html.Audio(id='audiospeler',
+               src='data:audio/wav;base64,{}'.format(data_sound.decode()),
+               controls=True,
+               autoPlay=False,
+               style={"width": "100%"}
+               )
+        return audio3 , plt
 
 
 if __name__ == '__main__':
